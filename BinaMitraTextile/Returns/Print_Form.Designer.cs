d4419d2fd177ff7bbc54bb473a66bb1936d8caef
RQ <ricky.dwsystems@gmail.com>
2022-07-16 15:09:45 +0700

    update from previous fix

--- BinaMitraTextile/Returns/Print_Form.Designer.cs
+++ BinaMitraTextile/Returns/Print_Form.Designer.cs
@@ -138,7 +138,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(663, 14);
+            this.label2.Location = new System.Drawing.Point(737, 14);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(222, 20);
             this.label2.TabIndex = 119;
